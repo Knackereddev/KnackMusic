dbe417c55c82b83ccff4171bd6cfde5be1b1e76b
Knackereddev <109649335+Knackereddev@users.noreply.github.com>
2025-06-25 12:40:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
